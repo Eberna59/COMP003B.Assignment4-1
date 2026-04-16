0e1239e4255f5220ae3b88dcfe5c4923fc5389f5
evanbern59 <evansantos1202@gmail.com>
2026-04-16 16:10:17 -0700

    Add project files.

---
+++ COMP003B.Assignment4-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.Assignment4_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
