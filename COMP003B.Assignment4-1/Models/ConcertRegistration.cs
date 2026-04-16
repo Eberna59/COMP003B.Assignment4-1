8ab857be09b0f3daace1fd011fe078daf52d712a
evanbern59 <evansantos1202@gmail.com>
2026-04-16 16:23:05 -0700

    added the concert registration model

---
+++ COMP003B.Assignment4-1/Models/ConcertRegistration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4_1.Models
+{
+    public class ConcertRegistration
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Range(1, 8)]
+        public int TicketCount { get; set; }
+    }
+}
