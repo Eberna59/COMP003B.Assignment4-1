eebdc05b60af8bb92a966ab4e1a153c4f03dff71
evanbern59 <evansantos1202@gmail.com>
2026-04-16 16:16:44 -0700

    added the song.cs models

---
+++ COMP003B.Assignment4-1/Models/Song.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4_1.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Album { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Year { get; set; }
+    }
+}
