8f6e16447744ee3ed597a923b5445d9e3698a3a7
evanbern59 <evansantos1202@gmail.com>
2026-04-16 18:40:15 -0700

    added songs file with index/albums/details html

---
+++ COMP003B.Assignment4-1/Views/Songs/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4_1.Views.Songs
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
