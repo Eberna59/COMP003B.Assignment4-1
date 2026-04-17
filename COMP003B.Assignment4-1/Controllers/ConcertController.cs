103301df07a5f460618e863b0940499e855d3a6d
evanbern59 <evansantos1202@gmail.com>
2026-04-16 18:06:25 -0700

    added songcontroller and concertcontroller

---
+++ COMP003B.Assignment4-1/Controllers/ConcertController.cs
@@ -0,0 +1,58 @@
+﻿using COMP003B.Assignment4_1.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4_1.Controllers
+{
+    [Route("concert")]
+    public class ConcertController : Controller
+    {
+        [HttpGet("register/{city?}")]
+        public IActionResult Register(string? city)
+        {
+            var model = new ConcertRegistration
+            {
+                City = city ?? "",
+                TicketCount = 1
+            };
+
+            ViewData["Title"] = "Register";
+            return View(model);
+        }
+
+        [HttpPost("register")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(ConcertRegistration model)
+        {
+            ViewData["Title"] = "Register";
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return RedirectToAction("Confirmation", new
+            {
+                id = 1,
+                fullName = model.FullName,
+                city = model.City,
+                ticketCount = model.TicketCount
+            });
+        }
+
+        [HttpGet("confirmation/{id:int}")]
+        public IActionResult Confirmation(int id, string fullName, string city, int ticketCount)
+        {
+            var model = new ConcertRegistration
+            {
+                Id = id,
+                FullName = fullName,
+                City = city,
+                TicketCount = ticketCount
+            };
+
+            ViewData["Title"] = "Confirmation";
+            return View(model);
+        }
+    }
+}
---
+++ COMP003B.Assignment4-1/Controllers/SongsController.cs
@@ -0,0 +1,49 @@
+﻿using COMP003B.Assignment4_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System.Reflection;
+
+namespace COMP003B.Assignment4_1.Controllers
+{
+    public class SongsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var songs = new List<Song>
+            {
+                new Song { Id = 1, Title = "Monochromatic", Album = "Single", Description = "My favorite song", Year = "2025"},
+                new Song { Id = 2, Title = "...And to Those I Love, Thanks for Sticking around", Album = "Single", Description = "Number one Selling song of the Year", Year = "2020"}
+            };
+
+            ViewData["Title"] = "Songs";
+            return View(songs);
+        }
+
+        public IActionResult Albums()
+        {
+            var songs = new List<Song>
+            {
+                new Song { Id = 3, Title = "THY WILL BE DONE", Album = "Recent Album", Description = "Best Seller of 2025", Year = "2025" },
+                new Song { Id = 4, Title = "New World Depression", Album = "Best Selling Album", Description = "Best Seller of 2024", Year = "2024" },
+            };
+
+            ViewData["Title"] = "Albums";
+            return View(songs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var song = new Song
+            {
+                Id = id,
+                Title = "Sample Song",
+                Album = "Sample Album",
+                Description = "Sample description",
+                Year = "Sample Year"
+            };
+
+            ViewData["Title"] = "Details";
+            return View(song);
+        }
+    }
+}
